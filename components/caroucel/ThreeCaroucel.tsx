7c205b76adb2fb8f2a940b0f79a48e7793d43243
iinsue <hinsu93@gmail.com>
2023-07-06 23:56:27 +0900

    Three Caroucel 기본설정

--- components/caroucel/ThreeCaroucel.tsx
+++ components/caroucel/ThreeCaroucel.tsx
@@ -1,9 +1,16 @@
 "use client";
 
+import { motion, AnimatePresence } from "framer-motion";
+
+const variants = {};
+
 const ThreeCaroucel = () => {
   return (
     <div className="bg-slate-500 w-96 h-64">
       <div>ThreeCaroucel</div>
+      <AnimatePresence>
+        <motion.div></motion.div>
+      </AnimatePresence>
     </div>
   );
 };
