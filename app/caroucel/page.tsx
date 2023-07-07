b29a7104049c326f716c9bc25aa20e02259c1f12
iinsue <hinsu93@gmail.com>
2023-07-07 23:34:04 +0900

    캐러셀 중간저장

--- app/caroucel/page.tsx
+++ app/caroucel/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import FourCaroucel from "@/components/caroucel/FourCaroucel";
 import OneCaroucel from "@/components/caroucel/OneCaroucel";
 import ThreeCaroucel from "@/components/caroucel/ThreeCaroucel";
 import TwoCaroucel from "@/components/caroucel/TwoCaroucel";
@@ -8,8 +9,9 @@ const CaroucelPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       {/*  <OneCaroucel /> */}
-      <TwoCaroucel />
+      {/* <TwoCaroucel /> */}
       {/* <ThreeCaroucel /> */}
+      <FourCaroucel />
     </div>
   );
 };
