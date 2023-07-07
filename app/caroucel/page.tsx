b2a76314e07b2a44a9107ae38609b7a17d46898f
iinsue <hinsu93@gmail.com>
2023-07-07 22:34:53 +0900

    기본 캐러셀

--- app/caroucel/page.tsx
+++ app/caroucel/page.tsx
@@ -2,12 +2,14 @@
 
 import OneCaroucel from "@/components/caroucel/OneCaroucel";
 import ThreeCaroucel from "@/components/caroucel/ThreeCaroucel";
+import TwoCaroucel from "@/components/caroucel/TwoCaroucel";
 
 const CaroucelPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <OneCaroucel />
-      <ThreeCaroucel />
+      {/*  <OneCaroucel /> */}
+      <TwoCaroucel />
+      {/* <ThreeCaroucel /> */}
     </div>
   );
 };
