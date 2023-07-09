34cc51bd4bb04d320f299e1c126a463d4d22cdff
iinsue <hinsu93@gmail.com>
2023-07-09 10:21:39 +0900

    오타 수정

--- components/caroucel/FourCaroucel.tsx
+++ components/caroucel/FourCaroucel.tsx
@@ -30,16 +30,16 @@ const variants = {
     opacity: 1,
     transition: {
       x: { type: "spring", stiffness: 300, damping: 30 },
-      opcaity: { duration: 0.2 },
+      opacity: { duration: 0.2 },
     },
   },
   exit: (direction: any) => {
     return {
       x: direction > 0 ? -1000 : 1000,
-      opcaity: 0,
+      opacity: 0,
       transition: {
         x: { type: "spring", stiffness: 300, damping: 30 },
-        opacity: { duration: 0.2 },
+        opacity: { duration: 0.4 },
       },
     };
   },
--- components/caroucel/NoAnimateOne.tsx
+++ components/caroucel/NoAnimateOne.tsx
@@ -1,5 +1,24 @@
 "use client";
 
+import { motion, AnimatePresence } from "framer-motion";
+
+const variants = {
+  initial: (direction: any) => {
+    return {
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0,
+    };
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      x: { type: "spring", stiffness: 300, damping: 30 },
+      opacity: { duration: 0.2 },
+    },
+  },
+};
+
 const NoAnimateOne = () => {
   return <div></div>;
 };
