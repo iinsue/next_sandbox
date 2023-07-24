7934c10fcae1579a157d82e3ae6c17970781542e
iinsue <hinsu93@naver.com>
2023-07-24 23:36:23 +0900

    useWindow 훅 추가

---
+++ hook/useWindow.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+function getWindowSize() {
+  const { innerWidth: width } = window;
+  return width;
+}
+
+// 창 크기가 변경되면 다시 갱신하도록 하는 함수
+function useWindow() {
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+  useEffect(() => {
+    function handleWindowSize() {
+      setWindowSize(getWindowSize());
+    }
+    window.addEventListener("resize", handleWindowSize);
+    return () => window.removeEventListener("resize", handleWindowSize);
+  }, []);
+  return windowSize;
+}
+
+export default useWindow;
