1c2da87e38cf257d340ae303c0f04280fd6f7a70
iinsue <hinsu93@gmail.com>
2023-11-27 00:27:43 +0900

    Lexical - Add Plugin My Custom Auto Focus Plugin

--- app/board/lexical/plain/_components/editor.tsx
+++ app/board/lexical/plain/_components/editor.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import { $getRoot, $getSelection } from "lexical";
-import { useEffect } from "react";
 
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
-import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+import MyCustomAutoFocusPlugin from "./plugins/MyCustomAutoFocusPlugin";
 
 const theme = {
   ltr: "ltr",
@@ -15,23 +14,6 @@ const theme = {
   placeholder: "editor-placeholder",
   paragraph: "editor-paragraph",
 };
-
-/* 
-Lexical React plugins are React components, which makes them
-hgihly composable. Furthermore, you can lazy load plugins if
-desired, so you don't pay the cost for plugins until you
-actually use them.
-*/
-function MyCustomAutoFocusPlugin() {
-  const [editor] = useLexicalComposerContext();
-
-  useEffect(() => {
-    // Focus the eidtor when the effect fires!
-    editor.focus();
-  }, [editor]);
-
-  return null;
-}
 
 /* 
 Catch any errors that occur during Lexical updates and log them or
