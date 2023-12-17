82006ab04349575681a2f4576e27fc963964bf9f
iinsue <hinsu93@gmail.com>
2023-12-17 22:07:26 +0900

    Tanstack - Virtual Example Link 만들기

--- app/virtual/page.tsx
+++ app/virtual/page.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const VirtualMain = () => {
   return (
     <>
-      <div>VirtualMain</div>
+      <div className="bg-slate-100 h-full flex flex-col items-center pt-10">
+        <Link href="/virtual/fixed">
+          <Button variant="link">Tanstack Virtual Fixed Exmaple</Button>
+        </Link>
+        <Link href="/virtual/variable">
+          <Button variant="link">Tanstack Virtaul Variable Example</Button>
+        </Link>
+      </div>
     </>
   );
 };
