71d251969c525d909ab3e16dbc2c0e93e7be2b0e
iinsue <hinsu93@naver.com>
2023-12-24 00:54:03 +0900

    Virtual - Add variable Example Main

--- app/virtual/variable/page.tsx
+++ app/virtual/variable/page.tsx
@@ -1,7 +1,33 @@
+"use client";
+
+import { useVirtualizer } from "@tanstack/react-virtual";
+
+const rows = new Array(10000)
+  .fill(true)
+  .map(() => 25 + Math.round(Math.random() * 100));
+
+const columns = new Array(10000)
+  .fill(true)
+  .map(() => 75 + Math.round(Math.random() * 100));
+
 const VirtualVariablePage = () => {
   return (
     <>
-      <div>VirtualVariablePage</div>
+      <div className="bg-slate-50 h-full flex justify-center">
+        <div>
+          <p>
+            These components are using <strong>variable</strong> sizes. This
+            means that each element has a unique, but knowable dimension at
+            render time.
+          </p>
+          <br />
+          <br />
+          <h3>Rows</h3>
+          <br />
+          <br />
+          <h3>Columns</h3>
+        </div>
+      </div>
     </>
   );
 };
