030a5a462fb237c44f032cf3ce52b91f8ee7fe41
iinsue <hinsu93@naver.com>
2023-12-24 00:19:43 +0900

    Snab - Add Exmaple Page

--- app/snab/page.tsx
+++ app/snab/page.tsx
@@ -1,7 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const SnabMainPage = () => {
   return (
     <>
-      <div>Snab Main</div>
+      <div className="flex flex-col h-full items-center pt-8 bg-slate-50">
+        <Link href="/snab/tutorial">
+          <Button variant="link">Tutorial</Button>
+        </Link>
+        <Link href="/snab/subtitle">
+          <Button variant="link">Subtitle</Button>
+        </Link>
+        <Link href="/snab/translation">
+          <Button variant="link">Translation</Button>
+        </Link>
+      </div>
     </>
   );
 };
