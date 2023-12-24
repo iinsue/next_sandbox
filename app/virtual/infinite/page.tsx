46fa4232e1200ead6693372c2190acd94cca4e97
iinsue <hinsu93@naver.com>
2023-12-24 16:14:27 +0900

    Virtaul - Error Fixed

--- app/virtual/infinite/page.tsx
+++ app/virtual/infinite/page.tsx
@@ -1,7 +1,127 @@
+"use client";
+
+import { QueryClient, useInfiniteQuery } from "@tanstack/react-query";
+import { useVirtualizer } from "@tanstack/react-virtual";
+import { Fragment, useEffect, useRef } from "react";
+
+const queryClient = new QueryClient();
+
+async function fetchServerPage(
+  limit: number,
+  offset: number = 0
+): Promise<{ rows: string[]; nextOffset: number }> {
+  const rows = new Array(limit)
+    .fill(0)
+    .map((e, i) => `Async loaded row #${i + offset * limit}`);
+
+  await new Promise((r) => setTimeout(r, 500));
+
+  return { rows, nextOffset: offset + 1 };
+}
+
 const InfiniteScrollPage = () => {
+  const {
+    status,
+    data,
+    error,
+    isFetching,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["projects"],
+    queryFn: (ctx) => fetchServerPage(10, ctx.pageParam),
+    getNextPageParam: (_lastGroup, groups) => groups.length,
+    initialPageParam: 0,
+  });
+
+  const allRows = data ? data.pages.flatMap((d) => d.rows) : [];
+
+  const parentRef = useRef<HTMLDivElement>(null);
+
+  const rowVirtualizer = useVirtualizer({
+    count: hasNextPage ? allRows.length + 1 : allRows.length,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 100,
+    overscan: 5,
+  });
+
+  useEffect(() => {
+    const [lastItem] = [...rowVirtualizer.getVirtualItems()].reverse();
+
+    if (!lastItem) {
+      return;
+    }
+
+    if (
+      lastItem.index >= allRows.length - 1 &&
+      hasNextPage &&
+      !isFetchingNextPage
+    ) {
+      fetchNextPage();
+    }
+  }, [
+    hasNextPage,
+    fetchNextPage,
+    allRows.length,
+    isFetchingNextPage,
+    rowVirtualizer.getVirtualItems(),
+  ]);
+
   return (
     <>
       <div>Infinite Scroll Page</div>
+      {status === "pending" ? (
+        <p>Loading...</p>
+      ) : status === "error" ? (
+        <span>Error:{(error as Error).message}</span>
+      ) : (
+        <div
+          ref={parentRef}
+          className="border border-[#e6e4dc] max-w-full"
+          style={{
+            height: `500px`,
+            width: `100%`,
+            overflow: "auto",
+          }}
+        >
+          <div
+            style={{
+              height: rowVirtualizer.getTotalSize(),
+              width: "100%",
+              position: "relative",
+            }}
+          >
+            {rowVirtualizer.getVirtualItems().map((virtualRow) => {
+              const isLoaderRow = virtualRow.index > allRows.length - 1;
+              const post = allRows[virtualRow.index];
+
+              return (
+                <div
+                  key={virtualRow.index}
+                  style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: virtualRow.size,
+                    transform: `translateY(${virtualRow.start}px)`,
+                  }}
+                >
+                  {isLoaderRow
+                    ? hasNextPage
+                      ? "Loading more...."
+                      : "Nothing more to load"
+                    : post}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
+      <div>
+        {isFetching && isFetchingNextPage ? "Background Updating..." : null}
+      </div>
     </>
   );
 };
