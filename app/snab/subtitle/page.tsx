b791b76162b9aace64ad5d32f960c9879c142a54
Insu Hong <hinsu93@naver.com>
2023-12-26 16:50:37 +0900

    Snab - Add Subtitle Outline

--- app/snab/subtitle/page.tsx
+++ app/snab/subtitle/page.tsx
@@ -1,7 +1,70 @@
+"use client";
+
+import { useRef, useState } from "react";
+import { makeData, Subtitle } from "./_components/makeData";
+import { useVirtualizer } from "@tanstack/react-virtual";
+
 const SubtitlePage = () => {
+  const [data, setData] = useState(makeData(2000));
+
+  // React Virtual 을 위한 Virtual Ref
+  const parentRef = useRef<HTMLDivElement>(null);
+
+  // React Virtual Hook
+  const virtualizer = useVirtualizer({
+    count: data.length,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 60,
+  });
+
+  // React VirtualItem
+  const virtualItems = virtualizer.getVirtualItems();
+
   return (
     <>
-      <div>Subtitle Page</div>
+      <div className="w-[400px] h-[300px]">
+        <div
+          ref={parentRef}
+          className="w-full h-full max-w-full bg-slate-200"
+          style={{
+            contain: "strict",
+            overflow: "auto",
+          }}
+        >
+          <div
+            className="w-full"
+            style={{
+              height: virtualizer.getTotalSize(),
+              position: "relative",
+            }}
+          >
+            {virtualItems.map((virtualRow) => (
+              <div
+                key={virtualRow.key}
+                ref={virtualizer.measureElement}
+                data-index={virtualRow.index}
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  width: "100%",
+                  transform: `translateY(${virtualItems[0]?.start ?? 0}px)`,
+                }}
+              >
+                {virtualItems.map((virtualRow) => (
+                  <div
+                    key={virtualRow.key}
+                    ref={virtualizer.measureElement}
+                    data-index={virtualRow.index}
+                  >
+                    <div>{data[virtualRow.index].transcription}</div>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
