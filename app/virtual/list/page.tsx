52967a01b97646bd62e59da907aaba8d9f147e3a
iinsue <hinsu93@gmail.com>
2023-12-31 21:47:49 +0900

    Virtualized - Update List Exmaple

--- app/virtual/list/page.tsx
+++ app/virtual/list/page.tsx
@@ -1,7 +1,71 @@
+"use client";
+
+import { loremIpsum } from "lorem-ipsum";
+import { useEffect, useState } from "react";
+import { List } from "react-virtualized";
+
+const rowCount = 5000;
+const listHeight = 400;
+const rowHeight = 50;
+const rowWidth = 700;
+
+const list = Array(rowCount)
+  .fill(undefined)
+  .map((val, idx) => {
+    return {
+      id: idx,
+      name: "John Doe",
+      image: "http://via.placeholder.com/40",
+      text: loremIpsum({
+        count: 1,
+        units: "sentences",
+        sentenceLowerBound: 4,
+        sentenceUpperBound: 8,
+      }),
+    };
+  });
+
+const renderRow = ({ index, key, style }: any) => {
+  return (
+    <div
+      key={key}
+      style={style}
+      className="border-b border-[#ebeced] mx-[5px] my-0 flex items-center"
+    >
+      <div className="mr-[10px]">
+        <img src={list[index].image} alt="" />
+      </div>
+      <div className="p-[10px]">
+        <div>{list[index].name}</div>
+        <div>{list[index].text}</div>
+      </div>
+    </div>
+  );
+};
+
 const VirtualizedListPage = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (isMounted === false) return;
+
   return (
     <>
-      <div>Virtualized List Page</div>
+      <div>
+        <div className="p-[10px]">
+          <List
+            width={rowWidth}
+            height={listHeight}
+            rowHeight={rowHeight}
+            rowRenderer={renderRow}
+            rowCount={list.length}
+            overscanRowCount={3}
+          />
+        </div>
+      </div>
     </>
   );
 };
