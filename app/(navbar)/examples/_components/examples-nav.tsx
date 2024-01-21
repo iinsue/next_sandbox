e3faf5c378622d93e397c59b43b1da548c346f8d
iinsue <hinsu93@gmail.com>
2024-01-21 19:30:59 +0900

    Add Forms & Music Page

--- app/(navbar)/examples/_components/examples-nav.tsx
+++ app/(navbar)/examples/_components/examples-nav.tsx
@@ -13,6 +13,16 @@ const examples = [
     href: "/examples/mail",
     code: "https://github.com/shadcn/ui/tree/main/apps/www/app/examples/mail",
   },
+  {
+    name: "Forms",
+    href: "/examples/forms",
+    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/examples/forms",
+  },
+  {
+    name: "Music",
+    href: "/examples/music",
+    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/examples/music",
+  },
 ];
 
 interface ExamplesNavProps extends React.HTMLAttributes<HTMLDivElement> {}
@@ -22,14 +32,24 @@ export const ExamplesNav = ({ className, ...props }: ExamplesNavProps) => {
 
   return (
     <div className="relative">
-      <ScrollArea>
-        <div>
+      <ScrollArea className="max-w-[600px] lg:max-w-none">
+        <div className={cn("mb-4 flex items-center", className)} {...props}>
           {examples.map((example, index) => (
-            <Link href={example.href} key={example.href}>
+            <Link
+              href={example.href}
+              key={example.href}
+              className={cn(
+                "flex h-7 items-center justify-center rounded-full px-4 text-center text-sm transition-colors hover:text-primary",
+                pathname?.startsWith(example.href)
+                  ? "bg-muted font-medium text-primary"
+                  : "text-muted-foreground"
+              )}
+            >
               {example.name}
             </Link>
           ))}
         </div>
+        <ScrollBar orientation="horizontal" className="invisible" />
       </ScrollArea>
     </div>
   );
---
+++ app/(navbar)/examples/forms/page.tsx
@@ -0,0 +1,9 @@
+const FormsPage = () => {
+  return (
+    <>
+      <div>Forms Page</div>
+    </>
+  );
+};
+
+export default FormsPage;
