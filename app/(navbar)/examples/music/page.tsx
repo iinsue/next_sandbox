5f6f12d83f6770925d36bea430090affc365d948
iinsue <hinsu93@gmail.com>
2024-01-21 20:45:04 +0900

    Add Tab page

--- app/(navbar)/examples/music/page.tsx
+++ app/(navbar)/examples/music/page.tsx
@@ -1,7 +1,25 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 const MusicPage = () => {
   return (
     <>
-      <div>Music Page</div>
+      <div>Music Image</div>
+      <div>
+        <Tabs>
+          <div>
+            <TabsList>
+              <TabsTrigger value="music">Music</TabsTrigger>
+              <TabsTrigger value="podcasts">Podcasts</TabsTrigger>
+              <TabsTrigger value="live">Live</TabsTrigger>
+            </TabsList>
+            <div>
+              <TabsContent value="music">MusicContents</TabsContent>
+              <TabsContent value="podcasts">PodCastContents</TabsContent>
+              <TabsContent value="live">LiveContents</TabsContent>
+            </div>
+          </div>
+        </Tabs>
+      </div>
     </>
   );
 };
