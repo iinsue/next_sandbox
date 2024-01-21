cff5d7889b00dadc63661627cda30bda0670a1e6
iinsue <hinsu93@gmail.com>
2024-01-21 19:01:22 +0900

    Update Example Layout

--- app/(navbar)/examples/layout.tsx
+++ app/(navbar)/examples/layout.tsx
@@ -5,7 +5,12 @@ import { cn } from "@/lib/utils";
 
 import { buttonVariants } from "@/components/ui/button";
 import { Announcement } from "./_components/announcement";
-import { PageHeader, PageHeaderHeading } from "./_components/page-header";
+import {
+  PageHeader,
+  PageHeaderHeading,
+  PageHeaderDescription,
+  PageAction,
+} from "./_components/page-header";
 
 export const metadata: Metadata = {
   title: "Examples",
@@ -26,9 +31,29 @@ export default function ExampleLayout({ children }: ExamplesLayoutProps) {
             Check out some examples
           </PageHeaderHeading>
           <PageHeaderHeading className="md:hidden">Examples</PageHeaderHeading>
+          <PageHeaderDescription>
+            Dashboard, cards, authentication. Some examples built using the
+            components. Use this as a guide to build your own.
+          </PageHeaderDescription>
+          <PageAction>
+            <Link href="" className={cn(buttonVariants(), "rounded-[6px]")}>
+              Get Started
+            </Link>
+            <Link
+              href=""
+              className={cn(
+                buttonVariants({ variant: "outline" }),
+                "rounded-[6px]"
+              )}
+            >
+              Components
+            </Link>
+          </PageAction>
         </PageHeader>
         <section>
-          <div>{children}</div>
+          <div className="overflow-hidden rounded-[0.5rem] border bg-background shadow-md md:shadow-xl">
+            {children}
+          </div>
         </section>
       </div>
     </>
